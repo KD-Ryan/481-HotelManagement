9984a5bc2f4c5bb8e0fd16dba386f0aa4c1edc10
KD-Ryan <ryanaguilar010@hotmail.com>
2025-09-24 18:33:46 -0700

    Add files via upload

---
+++ HotelMain.java
@@ -0,0 +1,14 @@
+//CSC481 Software Engineering
+//Hotel Housekeeping Management Software
+//Main Java File
+
+//Import whatever is needed above this comment ^
+
+public class HotelMain
+{
+   public static void main(String[] args)
+   {
+      //Everything we need will be added onto here as needed.
+   }
+   
+}
